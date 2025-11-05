70928b1f4fa6dd99dee5cbee7788e28a1f0f85b7
Ronald.Zhang <Ronald.Zhang@demolab.jolera.com>
2025-11-05 16:55:41 -0500

    User Entity

--- MusicApp.Domain/Entities/User.cs
+++ MusicApp.Domain/Entities/User.cs
@@ -8,9 +8,33 @@ public class User {
     public required string DisplayName { get; set; }
     public required string PasswordHash { get; init; }
     public UserRole Role { get; set; }
-
     // Spotify Integration
     public string? SpotifyId { get; set; }
     public ArtistProfile? ArtistProfile { get; set; }
+
+    private User() { }
+    public static User Create(string email, string displayName, string passwordHash, string? spotifyId, UserRole role = UserRole.Listener) {
+        return new User {
+            Id = Guid.NewGuid(),
+            DisplayName = displayName,
+            Email = email,
+            SpotifyId = spotifyId,
+            Role = role,
+            PasswordHash = passwordHash,
+            ArtistProfile = new ArtistProfile()
+        };
+    }
+
+    public void UpdateArtistProfile(ArtistProfile artistProfile) {
+        ArtistProfile = artistProfile;
+    }
+
+    public void UpdateRole(UserRole role) {
+        Role = role;
+    }
+
+    public void UpdateSpotifyId(string spotifyId) {
+        SpotifyId = spotifyId;
+    }
 
 }
