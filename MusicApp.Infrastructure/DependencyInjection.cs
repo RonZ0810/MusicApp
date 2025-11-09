d3f0962e34169ff91115c08799a13cf727a32e9e
Ronald Zhang <ronzhang0810@gmail.com>
2025-11-09 11:31:42 -0500

    Cleaned Up some files

--- MusicApp.Infrastructure/DependencyInjection.cs
+++ MusicApp.Infrastructure/DependencyInjection.cs
@@ -9,8 +9,7 @@ public static class DependencyInjection {
     public static IServiceCollection AddInfrastructure(
         this IServiceCollection services,
         IConfiguration configuration) {
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
 
         return services;
     }
